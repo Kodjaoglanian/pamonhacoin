ca10b7ece6b6876479f0d6d4da915af2e7140a32
Bruno Kodjaoglanian <79171970+Kodjaoglanian@users.noreply.github.com>
2024-12-12 23:17:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-injectSpeedInsights();
-
 document.addEventListener('DOMContentLoaded', function() {
     const cardData = [
         { title: "Identidade brasileira e o apelo cultural", content: "" },
